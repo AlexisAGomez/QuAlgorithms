644998aac6c816ee29726efd79b34b8487d8d23a
Alexis A. Gomez <Alexis.Gomez@ieee.org>
2024-08-17 15:34:52 -0500

    Added new reference: Quantum Image Processing: A Mathematical Toolbox by Artyom M. Grigoryan and Sos Agaian

--- qu_algorithms/__init__.py
+++ qu_algorithms/__init__.py
@@ -4,7 +4,9 @@
 # Alexis A. Gomez, Dr. Artyom Grigoryan,
 # UTSA ECE, March 11, 2023
 #
-# Reference http://dx.doi.org/10.1117/12.3013940
+# Reference:
+#   http://dx.doi.org/10.1117/12.3013940
+#   Quantum Image Processing: A Mathematical Toolbox by Artyom M. Grigoryan and Sos Agaian
 
 # Import modules
 from qu_algorithms import qft
